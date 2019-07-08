1f5ba84d40e0fc6c2fe5f74e3da89b54721dbf7d
success <nkonwanasuccess@gmail.com>
2019-07-08 02:05:37 -0700

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './Component/home/home.component';
+import { AboutComponent } from './Component/about/about.component';
+import { AccomodationComponent } from './Component/accomodation/accomodation.component';
+import { GalleryComponent } from './Component/gallery/gallery.component';
+import { ContactComponent } from './Component/contact/contact.component';
+import { BookingComponent } from './Component/booking/booking.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    AccomodationComponent,
+    GalleryComponent,
+    ContactComponent,
+    BookingComponent
   ],
   imports: [
     BrowserModule,
